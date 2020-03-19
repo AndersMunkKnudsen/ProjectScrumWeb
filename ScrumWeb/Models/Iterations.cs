50760e8cc80daa7c2aa71c992b79bf66ca4de508
Anders Munk Knudsen - AMK <andersmunkk@hotmail.dk>
2020-03-19 10:43:44 +0100

    Fixed Iteration Start and End Date not showing in frontend. Model edited.

--- ScrumWeb/Models/Iterations.cs
+++ ScrumWeb/Models/Iterations.cs
@@ -11,14 +11,33 @@ namespace ScrumWeb.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Iterations
     {
         public string IterationID { get; set; }
         public string IterationName { get; set; }
         public string IterationDescription { get; set; }
-        public Nullable<System.DateTime> IterationStartDate { get; set; }
-        public Nullable<System.DateTime> IterationEndDate { get; set; }
+        //public Nullable<System.DateTime> IterationStartDate { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy/MM/dd}")]
+        [DataType(DataType.Date)]
+        public Nullable<DateTime> IterationStartDate
+        {
+            get;
+            set;
+        }
+
+        //public Nullable<System.DateTime> IterationEndDate { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy/MM/dd}")]
+        [DataType(DataType.Date)]
+        public Nullable<DateTime> IterationEndDate
+        {
+            get;
+            set;
+        }
+
         public string IterationProjectID { get; set; }
     }
 }
